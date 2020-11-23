dedbd46b44bf7bee5dbc30145f0f9e909d5c9a5e
lja3723 <lja3723@gmail.com>
2020-11-24 02:44:48 +0900

    배열 함수 사용코드 추가

--- main.c
+++ main.c
@@ -1,8 +1,22 @@
 #include "omok.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
 int main(void)
 {
 	//변경 테스트
+	int arr[15];
+
+	srand((unsigned)time(NULL));
+
+	for (int i = 0; i < 15; i++)
+		arr[i] = rand() % 30 + 1;
+
+	int sum = array_sum_int(arr, 15);
+
+	array_print(arr, 15);
+	printf("배열합: %d\n", array_sum_int(arr, 15));	
 
 	omok_start();
 	return 0;
